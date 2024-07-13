355debfecad340c4809371c76e700dac4fa38417
dekoms <dekoms@naver.com>
2024-07-13 14:54:50 +0900

    feat: remote 수정

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         >
           블로그 구경하기
         </a>
-        <a href="https://www.naver.com">네이버로 이동</a>
+        <a href="https://www.naver.com">네이버로 이동하자</a>
       </header>
     </div>
   );
