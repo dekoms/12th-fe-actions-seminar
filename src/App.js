c7d6bcef31f9ea951eced1f8d0846aa8104a6f61
dekoms <dekoms@naver.com>
2024-07-13 14:38:10 +0900

    feat: Actions Repository secrets 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>멋쟁이사자처럼 영남대학교 12기 대표 이진우</p>
+        <p>멋쟁이사자처럼 영남대학교 12기 운영진 이재훈</p>
         <a
           className="App-link"
           href="https://jinlee.kr/"
